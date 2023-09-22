4ac6a14264bf7fa7dd43a667a8e1e5bffb366ea8
Clement Mchar <clement.mchar@gmail.com>
2023-09-22 16:03:13 +0200

    création du script de scraping de la catégorie

--- scrape_category.py
+++ scrape_category.py
@@ -3,10 +3,14 @@ import csv
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+#J'importe les dépendances et je créé la fonction qui ajoute une ligne de produit au .csv
+
 def add_a_row(product, filename):
     with open(filename, "a", newline="", encoding="utf-8", delimiter=",") as fichier_csv :
         writer : csv.writer(fichier_csv)
         writer.writerow(product)
+
+#^Je n'écris pas l'en-tête ici sinon elle s'écrira à chaque itération de la fonction suivante
 
 def scrape_a_book(book_url) :
     reponse : requests.get(book_url)
@@ -27,5 +31,25 @@ def scrape_a_book(book_url) :
 
     add_a_row(list(product.values), "products.csv")
 
+#^La ligne du dessus appelle la fonction qui ajoute une ligne avec les valeurs de chaque livre au .csv final, sous forme de liste
 
 
+#Je créé la fonction qui permet d'itérer sur chaque url de chaque livre tant qu'il reste une page dans la catégorie, puis sur le bouton "next", jusqu'à ce qu'il n'y en ai plus
+
+def scrape_category(category_url):
+    while category_url :
+        reponse : requests.get(category_url)
+        soup : BeautifulSoup(reponse.content, "html.parser")
+
+        books_names = soup.find_all("h3")
+
+        for book_name in books_names :
+            lien = book_name.find("a")
+            if lien :
+                book_url = urljoin(category_url, lien["href"])
+                scrape_a_book(book_url)
+        next_button = soup.find("li", class_="next")
+        next_page_url = urljoin(category_url, next_button.find("a")["href"])        
+        category_url = next_page_url
+
+category_url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
