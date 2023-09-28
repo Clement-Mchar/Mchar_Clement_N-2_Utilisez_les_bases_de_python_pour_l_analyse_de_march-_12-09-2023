0c29f7e08956a04f3ba82fd092f3994005ba2342
Clement Mchar <clement.mchar@gmail.com>
2023-09-28 19:06:55 +0200

    Ajout du téléchargement des covers

--- scrape_all_books.py
+++ scrape_all_books.py
@@ -3,7 +3,9 @@ import csv
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import os
-
+import urllib.request
+import re
+import textwrap
 
 def add_a_row(product, filename):
     with open(filename, "a", newline="", encoding="utf-8") as fichier_csv :
@@ -14,24 +16,39 @@ def scrape_a_book(book_url, category_name) :
     reponse = requests.get(book_url)
     soup = BeautifulSoup(reponse.content, 'html.parser')
 
+    book_title = soup.find("h1").string
+    image_url = urljoin(book_url, soup.find("img")["src"])
+
     product = {
         "product_page_url": book_url + " ",
         "universal_product_code": soup.find('th', string="UPC").find_next('td').string,
-        "title": soup.find("h1").string,
+        "title": book_title,
         "price_including_tax": soup.find("th", string="Price (incl. tax)").find_next('td').string,
         "price_excluding_tax": soup.find("th", string="Price (excl. tax)").find_next('td').string,
         "number_available": soup.find("th", string="Availability").find_next('td').string,
         "product_description": soup.find("div", class_="sub-header").find_next('p').text.strip(),
         "category": category_name,
         "review_rating": soup.find("p", class_="star-rating")["class"][1],
-        "image_url": urljoin(book_url, soup.find("img")["src"])
+        "image_url": image_url
     }
 
     if not os.path.exists("Books"):
         os.mkdir("Books")
 
     category_filename = os.path.join("Books", f"{category_name}.csv")
+    
+    if not os.path.exists("Books_Covers") :
+        os.mkdir("Books_Covers")
 
+    book_title = soup.find("h1").text
+    clean_title = re.sub('[^A-Za-z0-9]+', " ", book_title).strip("'").replace("é", "e")
+    max_length = 115
+    shorter_title = textwrap.shorten(clean_title, width= max_length, placeholder="...")
+    image_name =  f"{shorter_title}.jpg"
+    image_path = os.path.join("Books_Covers", image_name)
+    
+    urllib.request.urlretrieve(image_url, image_path)
+    
     add_a_row(list(product.values()), category_filename)    
 
 def scrape_category(category_url):
